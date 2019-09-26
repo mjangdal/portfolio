e461236d9fcc58dc1e107e0305c56f51449f11e1
Mjangdal <martin.jangdal@ffcg.se>
2019-09-26 14:48:08 +0200

    klart

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,26 @@
 function checkForVisibility() {
   var rightBox = document.querySelectorAll(".right-animation");
   var boxLeft = document.querySelectorAll(".left-animation");
+  var topbox = document.querySelectorAll(".top-animation");
+  var lightbox = document.querySelectorAll(".mySlides");
+
+  topbox.forEach(function(top, lightbox){
+    if (isElementInViewport(top)){
+        top.classList.add("popin" );
+
+    }
+    else{
+        top.classList.remove("popin");
+        document.querySelector(".mySlides").classList.remove("lightbox");
+    }
+  });
 
   rightBox.forEach(function(right) {
     if (isElementInViewport(right)) {
       right.classList.add("visible");
     } else {
       right.classList.remove("visible");
+
     }
   });
   boxLeft.forEach(function(boxLeft) {
@@ -31,4 +45,12 @@ function isElementInViewport(el) {
 
 if (window.addEventListener) {
   addEventListener("scroll", checkForVisibility, false);
+  addEventListener("load", document.querySelector(".top-animation").classList.add("popin"))
 }
+
+function gallery(i){
+/*     document.getElementById("overlay").classList.add("overlay")
+ */    document.getElementById(i).classList.add("lightbox");
+    
+}
+
